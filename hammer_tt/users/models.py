7737b9bdedb136bb194aef09bea7c2de53d5cfa0
Yauheni Karaliou <evgeny.korolev555@gmail.com>
2024-12-04 11:45:36 +0100

    Created model, registered in admin panel and created migrations

--- hammer_tt/users/models.py
+++ hammer_tt/users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import string
+import random
 
-# Create your models here.
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, unique=True)
+    verification_code = models.CharField(max_length=4, blank=True, null=True)
+    invite_code = models.CharField(max_length=6, unique=True, blank=True, null=True)
+    activated_invite_code = models.CharField(max_length=6, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.invite_code:
+            self.invite_code = self.generate_invite_code()
+        super().save(*args, **kwargs)
+
+    def generate_invite_code(self):
+        characters = string.ascii_letters + string.digits
+        while True:
+            code = ''.join(random.choices(characters, k=6))
+            if not User.objects.filter(invite_code=code).exists():
+                return code
+
